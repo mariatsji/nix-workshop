d234f8efe2d188a880cfecf1d25477e3a3ae0aaf
Sjur Millidahl <sjur.millidahl@finn.no>
2022-01-07 11:04:09 +0100

    part1

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
-nix eval -f ./testrunner.nix res1
+nix eval -f ./testrunner.nix res1a
+nix eval -f ./testrunner.nix res1b
 nix eval -f ./testrunner.nix res2
 nix eval -f ./testrunner.nix res3
+nix eval -f ./testrunner.nix res4a
+nix eval -f ./testrunner.nix res4b
+nix eval -f ./testrunner.nix res4c
+nix eval -f ./testrunner.nix res5
--- testrunner.nix
+++ testrunner.nix
@@ -1,16 +1,36 @@
 let exercises = import ./exercises.nix;
 
 in with import ./utils/nest.nix; {
-    res1 = assertEquals
+    res1a = assertEquals
         "part1 a string sais it is a string"
-        exercises.part1
+        exercises.part1a
         "I am a string";
+    res1b = assertEquals
+        ''part1 a string sais it is a "string"''
+        exercises.part1b
+        ''I am a "string"'';
     res2 = assertEquals
-        "part1 a lambda function from x to x squared applied to 3"
+        "part2 a lambda function from x to x squared applied to 3"
         (exercises.part2 3)
         9;
     res3 = assertEquals
         "part3 point to another file"
         (builtins.readFile exercises.part3)
         ":)";
+    res4a = assertEquals
+        "part4 create a record is actually a set"
+        (builtins.typeOf exercises.part4)
+        "set";
+    res4b = assertEquals
+        "part4 create a set with key a"
+        exercises.part4.a
+        true;
+    res4c = assertEquals
+        "part4 create a set with key b"
+        exercises.part4.b
+        5;
+    res5 = assertEquals
+        "part5 create the map function for lists"
+        (exercises.part5 builtins.toString [ 1 2 3])
+        ["1" "2" "3"];
 }
