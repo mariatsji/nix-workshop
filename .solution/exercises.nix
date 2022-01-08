d354b828a7003ba96d91443a20a7c0df380091d0
Sjur Millidahl <sjur.millidahl@finn.no>
2022-01-08 20:27:09 +0100

    obfuscate solutions

--- .solution/exercises.nix
+++ .solution/exercises.nix
@@ -1,16 +1,18 @@
 # read about the (alas very few) nix types : https://nixos.wiki/wiki/Nix_Expression_Language#Types
 {
     # assign the string "I am a string" to the variable part1 inside this map
-    part1a = throw "part1a not implemented";
+    part1a = ''I am a string'';
     # assign the same string in part2a, but this time you should have quotes around "string"
-    part1b = throw "part1b not implemented";
+    part1b = ''I am a "string"'';
     # assign the function from a number to the square of that number to the variable part2
-    part2 = throw "part2 not implemented";
+    part2 = x: x * x;
     # assign a filepath to important.document.txt that lives in the doc folder
-    part3 = throw "part3 not implemented";
+    # ./docs/important.document.txt
+    part3 = ./docs/important.document.txt;
     # create a set containing two keys: a -> true, b -> 5
-    part4 = throw "part4 not implemented";
+    # { a=true; b=5;}
+    part4 = { a=true; b=5;};
     # assign an anonymous function that accepts a function (a -> b) and a list of a, returning a list of b
     # read about builtin functions that you can use here (use by e.g. builtins.all)
-    part5 = throw "part5 not implemented";
+    part5 = builtins.map;
 }
